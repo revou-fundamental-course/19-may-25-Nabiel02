afa3f2f220d918141b9eccb634f653230427f601
Nabiel02 <m.nabiel0611@gmail.com>
2025-05-29 17:16:29 +0700

    update js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log ('Javascript file loaded successfully');
+
+// Fungtion to display an alert when the button is clikced
+function validateInput{
+ const input suhu = document.getElementById('input-suhu') ;
+console.log(input-suhu.value) ;
+}
+
+// Validasi input suhu
+if (inputSuhu.value == '') {
+    alert('input tidak boleh kosong!');
+} else{
+    /// Convert input to a number
+}
